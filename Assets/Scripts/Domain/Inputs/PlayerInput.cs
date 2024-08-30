8eb4b864c60fe0a66ce71618bc6c4e19626ea94a
Alex <EastTem@gmail.com>
2024-08-30 15:27:18 +0300

    feat: multiple players
    each player has its own input

--- Assets/Scripts/Domain/Inputs/PlayerInput.cs
+++ Assets/Scripts/Domain/Inputs/PlayerInput.cs
@@ -5,9 +5,23 @@ namespace Game.Domain.Inputs
 {
 	public class PlayerInput : IMovementInput
 	{
+		private readonly MoveMap _moveMap;
+
+		public PlayerInput(MoveMap moveMap)
+		{
+			_moveMap = moveMap;
+		}
+
 		public Vector2 GetMovement()
 		{
-			return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+			var direction = new Vector2();
+
+			if (Input.GetKey(_moveMap.UpKey)) direction.y = 1;
+			if (Input.GetKey(_moveMap.DownKey)) direction.y = -1;
+			if (Input.GetKey(_moveMap.RightKey)) direction.x = 1;
+			if (Input.GetKey(_moveMap.LeftKey)) direction.x = -1;
+
+			return direction;
 		}
 	}
 }
--- Assets/Scripts/Installers/PlayerInstaller.cs
+++ Assets/Scripts/Installers/PlayerInstaller.cs
@@ -2,17 +2,20 @@
 using Game.Domain.Inputs;
 using Game.Domain.Movement;
 using Game.View.Movement;
+using UnityEngine;
 using Zenject;
 
 namespace Game.Installers
 {
 	public class PlayerInstaller : MonoInstaller
 	{
+		[SerializeField] private MoveMap _moveMap;
+
 		public override void InstallBindings()
 		{
 			Container.Bind<IEntity>().To<Player>().AsSingle();
-			var playerInput = new PlayerInput();
-			Container.Bind<IMovementInput>().FromInstance(playerInput).AsSingle();
+			Container.Bind<MoveMap>().FromInstance(_moveMap).AsSingle();
+			Container.Bind<IMovementInput>().To<PlayerInput>().AsSingle();
 			Container.BindInterfacesAndSelfTo<PlayerMovement>().AsSingle();
 			Container.Bind<MoveableView>().FromComponentsInHierarchy().AsSingle();
 		}
